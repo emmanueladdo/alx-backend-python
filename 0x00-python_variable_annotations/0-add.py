14b01e27b91d7aeaf0acd6cbfc7f4ae109b9c4d3
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-07 23:47:17 +0000

    adds two floats

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,5 +5,15 @@ Function Adds Two Floats
 
 
 def add(a: float, b: float) -> float:
-    sum = a + b
-    return sum
+    """
+    Adds two floating-point numbers and returns the result.
+
+    Parameters:
+    - a (float): The first floating-point number.
+    - b (float): The second floating-point number.
+
+    Returns:
+    float: The sum of the two input numbers.
+    """
+    result = a + b
+    return result
