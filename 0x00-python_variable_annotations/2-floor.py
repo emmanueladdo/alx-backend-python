94cfaf2f9a05d856d0d84c7aac84dd1bff2004de
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-08 00:34:35 +0000

    float to int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+The functio takes argument n
+Returns the floor of  float n
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Parameters:
+        float (float): The first string.
+
+    Returns:
+        floor: the floor of the float
+    """
+    return math.floor(n)
