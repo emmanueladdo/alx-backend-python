b56fdf55530917c5983dabd26985ca92874132fe
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-08 00:42:22 +0000

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,27 @@
+"""
+Variable Definitions
+"""
+
+# An integer with a value of 1
+a: int = 1
+"""
+a (int): An integer with a value of 1.
+"""
+
+# A float with a value of 3.14
+pi: float = 3.14
+"""
+pi (float): A float with a value of 3.14.
+"""
+
+# A boolean with a value of True
+i_understand_annotations: bool = True
+"""
+i_understand_annotations (bool): A boolean with a value of True.
+"""
+
+# A string with a value of “Holberton”
+school: str = "Holberton"
+"""
+school (str): A string with a value of "Holberton".
+"""
