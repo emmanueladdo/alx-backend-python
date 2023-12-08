6fb7998cad8059e6c2afde5920f9e2098b0570c0
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-08 00:20:14 +0000

    concats two stringd

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,13 +4,14 @@ Contaons function The
 Concates two string argument
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Parameters:
     str1: first
     str2: second
 
-    Returns: 
-    concationation of 
+    Returns:
+    concationation of
     """
     return str1 + str2
