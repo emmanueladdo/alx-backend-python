abc555ca87f39426fc8e0a2896442474628eac6c
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-08 00:54:58 +0000

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env pyhton3
+#!/usr/bin/env python3
 """
 Takes list as argument and return sum as float
- input_list of floats as argument and returns their sum as a float
 """
-from typing import List
-
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """adds a list of floats and return a float"""
-    if not input_list:
-        return 0
+    """
+    Calculate the sum of a list of floats.
+
+    Parameters:
+        input_list (List[float]): A list of floats.
+
+    Returns:
+        float: The sum of the elements in the input list.
+    """
     return sum(input_list)
-    
