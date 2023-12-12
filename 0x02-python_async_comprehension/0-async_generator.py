66464ae39536ed71a0107c7e75fb161af2cc9ded
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-12 16:26:14 +0000

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,6 +13,6 @@ async def async_generator():
     Asynchronous generator that yields random numbers
     between 0 and 10.
     """
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
