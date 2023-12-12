0b983133b6344e8ba2a8eb7d7e60d01d02618feb
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-12 16:18:43 +0000

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+The module is a coroutine called async_generator
+that takes no arguments
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronous generator that yields random numbers
+    between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
